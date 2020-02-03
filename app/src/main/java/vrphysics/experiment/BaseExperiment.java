c067bf889f535aa14fe647414c51ea4ae52b3a38
Sheggy <infilonic@users.noreply.github.com>
2020-02-03 08:12:46 +0000

    Make pause and start methods overridable

--- app/src/main/java/vrphysics/experiment/BaseExperiment.java
+++ app/src/main/java/vrphysics/experiment/BaseExperiment.java
@@ -5,11 +5,4 @@ import vrphysics.controls.SimulationControls;
 import processing.core.PApplet;
 
 public abstract class BaseExperiment extends PApplet implements ExperimentMetaData, SimulationControls {
-    public final void start() {
-
-    }
-
-    public final void pause() {
-
-    }
 }
