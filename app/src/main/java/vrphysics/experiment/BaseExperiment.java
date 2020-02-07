312cebb3675d5cb39da1e8103726411a8683a909
Sheggy <infilonic@users.noreply.github.com>
2020-02-07 12:31:32 +0000

    Update how an experiments exits

--- app/src/main/java/vrphysics/experiment/BaseExperiment.java
+++ app/src/main/java/vrphysics/experiment/BaseExperiment.java
@@ -6,9 +6,7 @@ import vrphysics.controls.SimulationControls;
 import processing.core.PApplet;
 
 public abstract class BaseExperiment extends PApplet implements ExperimentMetaData, SimulationControls {
-    protected MainActivity vrActivity;
-
     public final void exit() {
-        this.vrActivity.returnToMenu();
+        ((MainActivity) this.getActivity()).returnToMenu();
     }
 }
